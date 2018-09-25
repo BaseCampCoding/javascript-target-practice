4ef3da44fc55624c2147da825a16b02625307b9a
Nate Clark <natec425@gmail.com>
2018-09-25 10:02:51 -0500

    Add starter file for required JavaScript
    The provided file leads the student a bit more on how they should
    structure their solution. A solution to the first problem is provided
    to additional guidance on what a solution might look like, and
    a function stub for the next problem is provide to lead the student
    into a next step.

---
+++ targetPractice.js
@@ -0,0 +1,11 @@
+function setCol3() {
+    var targetElement = document.querySelector('#set-col-3');
+    targetElement.classList.add('col-3');
+}
+
+function backgroundSwap() {
+    // put your solution here
+}
+
+setCol3();
+backgroundSwap();
