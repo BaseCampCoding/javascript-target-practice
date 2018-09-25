d4f3ae7327d52d6ea9dffe8f894a663207b80cf0
Nate Clark <natec425@gmail.com>
2018-09-25 10:02:51 -0500

    Add starter file for required JavaScript
    The provided file leads the student a bit more on how they should
    structure their solution. It includes a DOMContentLoaded listener
    that should call solution functions.
    
    Also, a solution to the first problem is provided to additional
    guidance on what a solution might look like.

---
+++ targetPractice.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function hitTargets() {
+    // Add solution functions here
+    setCol3();
+});
+
+function setCol3() {
+    var targetElement = document.querySelector('#set-col-3');
+    targetElement.classList.add('col-3');
+}
