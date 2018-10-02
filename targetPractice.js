bbd562e7aa2e929c7683977ad18939676ab73dc9
Nate Clark <natec425@gmail.com>
2018-10-02 09:11:56 -0500

    Remove DOMContentLoaded event

--- targetPractice.js
+++ targetPractice.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', function hitTargets() {
-    // Add solution functions here
-    setCol3();
-});
-
 function setCol3() {
     var targetElement = document.querySelector('#set-col-3');
     targetElement.classList.add('col-3');
 }
+
+function backgroundSwap() {
+    // put your solution here
+}
+
+setCol3();
+backgroundSwap();
